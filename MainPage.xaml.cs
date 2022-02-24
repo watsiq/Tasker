0e45eedabfc7ecea09cf1ab1103bfe9e93b51583
watsiq <64712597+watsiq@users.noreply.github.com>
2022-02-24 14:06:02 +0700

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Tasker_App
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        RMQ rmq = new RMQ();
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void PageLoadConnect(object sender, RoutedEventArgs e)
+        {
+            rmq.InitRMQConnection();
+            rmq.CreateRMQConnection();
+            while (!rmq.connection.IsOpen) { /*loooping hingga koneksi terbuka*/ }
+            rmq.CreateRMQChannel("workQ");
+            while (!rmq.channel.IsOpen) { /*loooping hingga channel terbuka*/ }
+            if (rmq.connection.IsOpen && rmq.channel.IsOpen)
+            {
+                Button.IsEnabled = true;
+                Button1.IsEnabled = true;
+            }
+
+        }
+
+        private void CloseRMQ(object sender, RoutedEventArgs e)
+        {
+            rmq.Disconnect();
+            Button.IsEnabled = false;
+            Button1.IsEnabled = false;
+        }
+
+        private void SendData(object sender, RoutedEventArgs e)
+        {
+            rmq.SendMessage(TextBox.Text, TextBox1.Text);
+        }
+    }
+}
---
+++ RMQ.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using RabbitMQ.Client.Events;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tasker_App
+{
+    class RMQ
+    {
+        public ConnectionFactory connectionFactory;
+        public IConnection connection;
+        public IModel channel;
+
+        public void InitRMQConnection(string host = "cloudrmqserver.pptik.id", int port = 5672, string user = "tmdgdai",
+        string pass = "tmdgdai", string vhost = "/tmdgdai")
+        {
+            connectionFactory = new ConnectionFactory();
+            connectionFactory.HostName = host;
+            connectionFactory.Port = port;
+            connectionFactory.UserName = user;
+            connectionFactory.Password = pass;
+            connectionFactory.VirtualHost = vhost;
+        }
+
+        public void CreateRMQConnection()
+        {
+            connection = connectionFactory.CreateConnection();
+            //Console.WriteLine("Koneksi " + (connection.IsOpen ? "Berhasil!" : "Gagal!"));
+        }
+
+        public void CreateRMQChannel(string queue_name, string routingKey = "", string exchange_name = "")
+        {
+            if (connection.IsOpen)
+            {
+                channel = connection.CreateModel();
+                //Console.WriteLine("Channel " + (channel.IsOpen ? "Berhasil!" : "Gagal!"));
+            }
+
+            if (channel.IsOpen)
+            {
+                channel.QueueDeclare(queue: queue_name,
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+                //Console.WriteLine("Queue telah dideklarasikan..");
+            }
+        }               
+        public void SendMessage(string tujuan, string msg = "send")
+        {
+            byte[] responseBytes = Encoding.UTF8.GetBytes(msg);// konversi pesan dalam bentuk string menjadi byte
+
+            channel.BasicPublish(exchange: "",
+                                    routingKey: tujuan,
+                                    basicProperties: null,
+                                    body: responseBytes);
+            //Console.WriteLine("Pesan: '" + msg + "' telah dikirim.");
+        }
+
+        public void Disconnect()
+        {
+            channel.Close();
+            channel = null;
+            //Console.WriteLine("Channel ditutup!");
+            if (connection.IsOpen)
+            {
+                connection.Close();
+            }
+
+            //Console.WriteLine("Koneksi diputus!");
+            connection.Dispose();
+            connection = null;
+        }
+    }
+}
